95a41047d56d6171712acf59eb0c9f683065bdc1
Monjurul Huda Munna <munnapagol@gmail.com>
2020-03-10 04:04:04 +0600

    added Facebook social login format

--- routes/oauth-social.js
+++ routes/oauth-social.js
@@ -11,4 +11,19 @@ module.exports = app => {
   app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
     res.redirect('/home');
   });
+
+  /*
+  // @ Facebool social login Routes
+
+  // Redirect the user to Facebook for authentication.  When complete,
+  // Facebook will redirect the user back to the application at
+  //     /auth/facebook/callback
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+
+  // Facebook will redirect the user to this URL after approval.  Finish the
+  // authentication process by attempting to obtain an access token.  If
+  // access was granted, the user will be logged in.  Otherwise,
+  // authentication has failed.
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
+  */
 };
